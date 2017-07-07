6bb169a312320da640a03bbfbc7688d27660d7fc
jacob.p <jacob.p@kakaocorp.com>
2017-07-07 18:52:47 +0900

    change restcontroller

--- src/main/java/com/benly/coinbot/controller/DemoController.java
+++ src/main/java/com/benly/coinbot/controller/DemoController.java
@@ -1,15 +1,16 @@
 package com.benly.coinbot.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
 /**
  * Created by jacob.p on 2017. 7. 7..
  */
-@Controller
+@RestController
 public class DemoController {
+
     @RequestMapping("/keyboard")
     public Keyboard getKeyboard() {
         Keyboard keyboard = new Keyboard();
