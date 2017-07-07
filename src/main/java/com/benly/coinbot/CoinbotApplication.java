511657583a482587b0a157ccd63dd6b9b33dce55
jacob.p <jacob.p@kakaocorp.com>
2017-07-07 18:17:38 +0900

    spring boot first commit

---
+++ src/main/java/com/benly/coinbot/CoinbotApplication.java
@@ -0,0 +1,12 @@
+package com.benly.coinbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinbotApplication.class, args);
+	}
+}
